48e7188ee3618feaf90b59c1f9c787c580f90216
Jiaxu Su <sujiaxushanghai@gmail.com>
2023-07-23 19:58:08 -0700

    index.html

--- src/main/java/com/example/selfstudymvcai/configs/MvcConfig.java
+++ src/main/java/com/example/selfstudymvcai/configs/MvcConfig.java
@@ -2,6 +2,7 @@ package com.example.selfstudymvcai.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,10 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp().prefix("/WEB-INF/jsp/").suffix(".jsp");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sujiaxu
+  Date: 7/23/23
+  Time: 19:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>nihao shijie</h1>
+</body>
+</html>
